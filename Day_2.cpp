7fed872cce9d8b5ad7a800f368bcaa455a3584bd
Prateek Jain <jain22719@gmail.com>
2021-02-04 12:18:10 +0530

    Day 2

---
+++ Day_2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the solve function below.
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+    
+    double tax = (double)(tip_percent * meal_cost) / 100 + (double)(tax_percent * tip_percent) / 100;
+
+    int totalCost = (int) round(tax + meal_cost);
+    cout<<totalCost;
+
+}
+
+int main()
+{
+    double meal_cost;
+    cin >> meal_cost;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int tip_percent;
+    cin >> tip_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int tax_percent;
+    cin >> tax_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
+
